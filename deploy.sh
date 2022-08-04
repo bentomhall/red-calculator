7c45942a0ac09d1c1bc5b208ad7b89a03f2c3b4d
Ben Hall <bentomhall@gmail.com>
2022-08-04 16:58:00 -0700

    bust them caches, at least for the JS

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #/bin/zsh
-echo $(date +%d.%m.%y-%H:%M:%S) > ./dist/version.txt
+now=$(echo $(date +%d.%m.%y-%H:%M:%S) | md5)
+echo $now > ./dist/version.txt
+sed -e "s/{HASH}/$now/" ./dist/calculator.template.html > ./dist/calculator.html
 scp -i ~/.ssh/redbot.rsa -P 18765 ./dist/* u858-tssv5jetxizj@ssh.admiralbenbo.org:~/www/admiralbenbo.org/public_html/red-calculator
